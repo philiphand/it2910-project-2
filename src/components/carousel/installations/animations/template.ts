d7c920f5ab80b9982e9f674b8dad4f4b082d8e51
Håkon Harnes <account@harnes.me>
2020-09-13 12:27:22 +0200

    Adds initial music animation frequency ball

--- src/components/carousel/installations/animations/template.ts
+++ src/components/carousel/installations/animations/template.ts
@@ -1,11 +1,50 @@
 import { IInstallationInput } from "../../../../interfaces/installations"
+import { IRgbaColor } from "../../../../util/colors"
+import { ColorUtil } from "../../../../util/colors"
 
 export const templateDraw = (inputs: IInstallationInput, ctx: CanvasRenderingContext2D) => {
-    console.log(inputs.mediaHandler.getByteFrequencyData())
+    ctx.clearRect(0, 0, 1000, 1000)
+    
+    let data = inputs.mediaHandler.getByteFrequencyData()
+    drawFrequencyBars(data, ctx, 400,400)
+}
 
-    ctx.clearRect(0,0,1000,1000)
+//TODO: Replace with inputs
+const NUM_BARS = 256;
+const FROM_COLOR: IRgbaColor = { r: 0, g: 219, b: 222, a: .8 }
+const TO_COLOR: IRgbaColor = { r: 252, g: 0, b: 255, a: .8 }
+
+const drawFrequencyBars = (data: Uint8Array, ctx: CanvasRenderingContext2D, width: number, height: number) => {
+    let scaling = (Math.min(width, height) / 2) / 256
+
+    for (let i = 0; i < data.length; i++) {
+        let rotation = (360 / NUM_BARS)
+        let fill
+        if (i <= NUM_BARS / 2) {
+            let reach = data[i] * scaling + 20
+            fill = ColorUtil.rgbTweenString(FROM_COLOR, TO_COLOR, 2 * i / NUM_BARS, 0.8)
+            drawFrequencyTriangle(ctx, rotation, reach, fill)
+        }
+        else {
+            let reach = data[128 + (-i + 128)] * scaling + 20
+            fill = ColorUtil.rgbTweenString(TO_COLOR, FROM_COLOR, (i * 2 - 256) / NUM_BARS, 0.8)
+            drawFrequencyTriangle(ctx, rotation, reach, fill)
+        }
+    }
+}
+
+const drawFrequencyTriangle = (ctx: CanvasRenderingContext2D, rotation: number, reach: number, fill:string ) => {
     ctx.beginPath()
-    ctx.moveTo(0,0)
-    ctx.lineTo(300, 150)
-    ctx.stroke()
+    ctx.moveTo(200, 200)
+    ctx.lineTo(200+(400/320), 200 + reach)
+    ctx.lineTo(200-(400/320), 200 + reach)
+    ctx.lineTo(200, 200)
+    ctx.closePath()
+
+    ctx.fillStyle = fill
+    ctx.fill()
+
+    ctx.translate(200, 200)
+    ctx.rotate(((Math.PI) / 180)*rotation)
+    ctx.translate(-200, -200)
 }
