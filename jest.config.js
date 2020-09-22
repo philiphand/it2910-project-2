d2c034fa3068357de54738ea696b04e0adc7b461
Philip <philip.h.andersen@gmail.com>
2020-09-22 11:07:01 +0200

    #25 Removed deprecated cleanup-after-each

--- jest.config.js
+++ jest.config.js
@@ -13,7 +13,6 @@ module.exports = {
     // when using React Testing Library and adds special
     // extended assertions to Jest
     setupFilesAfterEnv: [
-      "@testing-library/react/cleanup-after-each",
       "@testing-library/jest-dom/extend-expect"
     ],
   
